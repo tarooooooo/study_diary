7be48d651290c5df9e88e622df1a3ff37762d7e1
tarooooooo <ryutaromaeda83@gmail.com>
2022-09-24 00:06:38 +0900

    circle ciテスト落ち確認

--- test/controllers/public/top_page_controller_test.rb
+++ test/controllers/public/top_page_controller_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class Public::TopPageControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert false
+  end
 end
