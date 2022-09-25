d3b43f4e5ef0f0587d2c7611df5dae6b71c71af9
tarooooooo <ryutaromaeda83@gmail.com>
2022-09-25 21:45:36 +0900

    add: test

--- test/controllers/public/learning_diaries_controller_test.rb
+++ test/controllers/public/learning_diaries_controller_test.rb
@@ -1,13 +1,4 @@
 require "test_helper"
 
 class Public::LearningDiariesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get public_learning_diaries_index_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get public_learning_diaries_new_url
-    assert_response :success
-  end
 end
