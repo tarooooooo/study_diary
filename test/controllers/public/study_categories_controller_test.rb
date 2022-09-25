6a81f5d6259d2f1f4a8baf4614c0abcf4c3d4751
tarooooooo <ryutaromaeda83@gmail.com>
2022-09-25 15:12:40 +0900

    no message

--- test/controllers/public/study_categories_controller_test.rb
+++ test/controllers/public/study_categories_controller_test.rb
@@ -1,18 +1,4 @@
 require "test_helper"
 
 class Public::StudyCategoriesControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get public_study_categories_show_url
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get public_study_categories_edit_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get public_study_categories_new_url
-    assert_response :success
-  end
 end
