6e13d30afeee015f33c8b61f72af3821f7b0e5b0
tarooooooo <ryutaromaeda83@gmail.com>
2022-09-29 20:08:25 +0900

    fix: toppage show

--- app/controllers/public/top_page_controller.rb
+++ app/controllers/public/top_page_controller.rb
@@ -1,19 +1,13 @@
 class Public::TopPageController < ::Public::BaseController
   def show
     @learning_diaries_chart_data = []
-    @day = (Date.current - 1)
-    @day_2 = (Date.current - 2)
-    @day_3 = (Date.current - 3)
-    @day_4 = (Date.current - 4)
-    @day_5 = (Date.current - 5)
-    current_user.study_categories.each do | study_category |
-      @learning_diaries_chart_data.push({ name: study_category.name, data: [
-        ["#{@day_4}", study_category.learning_diaries.where(study_day: @day_4).sum(:study_time) ],
-        ["#{@day_3}", study_category.learning_diaries.where(study_day: @day_3).sum(:study_time) ],
-        ["#{@day_2}", study_category.learning_diaries.where(study_day: @day_2).sum(:study_time) ],
-        ["#{@day}", study_category.learning_diaries.where(study_day: @day).sum(:study_time) ],
-        ["#{Date.current}", study_category.learning_diaries.where(study_day: Date.current).sum(:study_time) ],
-      ]})
+    if current_user&.study_categories
+      User.find(21).study_categories.each_with_index  do | study_category, number |
+        @learning_diaries_chart_data.push(
+          ["#{Date.current - number}", study_category.learning_diaries.where(study_day: (Date.current - number)).sum(:study_time) ]
+        )
+      end
+      @study_categories_count = current_user.study_categories.group(:name).count
     end
   end
 end
