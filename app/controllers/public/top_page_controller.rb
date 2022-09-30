567df55a34e155afc05eab396faf725b2d413514
tarooooooo <ryutaromaeda83@gmail.com>
2022-09-30 13:48:40 +0900

    fix: top_page show チャート用のデータを作成する

--- app/controllers/public/top_page_controller.rb
+++ app/controllers/public/top_page_controller.rb
@@ -1,14 +1,14 @@
 class Public::TopPageController < ::Public::BaseController
   def show
-    @weekly_study_time = []
+    @daily_study_time = []
     @learning_diaries_chart_data = []
     if current_user&.study_categories
       current_user.study_categories.each_with_index  do | study_category, number |
-        @weekly_study_time << ["#{Date.current - number}", study_category.learning_diaries.where(study_day: (Date.current - number)).sum(:study_time) ]
+        @daily_study_time.push(["#{Date.current - number}", study_category.learning_diaries.where(study_day: (Date.current - number)).sum(:study_time) ])
       end
 
       current_user.study_categories.each do | study_category |
-        @learning_diaries_chart_data.push({ name: study_category.name, data: @weekly_study_time })
+        @learning_diaries_chart_data.push({ name: study_category.name, data: @daily_study_time })
       end
       @study_categories_count = current_user.study_categories.group(:name).count
     end
