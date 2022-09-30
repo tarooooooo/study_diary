0b1224126aa77136402d85e7edbe25040873a01b
tarooooooo <ryutaromaeda83@gmail.com>
2022-09-30 09:57:01 +0900

    fix: toppage_controller show

--- app/controllers/public/top_page_controller.rb
+++ app/controllers/public/top_page_controller.rb
@@ -1,11 +1,14 @@
 class Public::TopPageController < ::Public::BaseController
   def show
+    @weekly_study_time = []
     @learning_diaries_chart_data = []
     if current_user&.study_categories
-      User.find(21).study_categories.each_with_index  do | study_category, number |
-        @learning_diaries_chart_data.push(
-          ["#{Date.current - number}", study_category.learning_diaries.where(study_day: (Date.current - number)).sum(:study_time) ]
-        )
+      current_user.study_categories.each_with_index  do | study_category, number |
+        @weekly_study_time << ["#{Date.current - number}", study_category.learning_diaries.where(study_day: (Date.current - number)).sum(:study_time) ]
+      end
+
+      current_user.study_categories.each do | study_category |
+        @learning_diaries_chart_data.push({ name: study_category.name, data: @weekly_study_time })
       end
       @study_categories_count = current_user.study_categories.group(:name).count
     end
