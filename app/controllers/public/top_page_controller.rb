1c4344f5360f4747824551af6221820f7864fc9a
tarooooooo <ryutaromaeda83@gmail.com>
2022-10-02 11:11:04 +0900

    カテゴリ未指定のチャート用データ作成

--- app/controllers/public/top_page_controller.rb
+++ app/controllers/public/top_page_controller.rb
@@ -1,16 +1,33 @@
 class Public::TopPageController < ::Public::BaseController
   def show
-    @daily_study_time = []
     @learning_diaries_chart_data = []
-    if current_user&.study_categories
-      current_user.study_categories.each_with_index  do | study_category, number |
-        @daily_study_time.push(["#{Date.current - number}", study_category.learning_diaries.where(study_day: (Date.current - number)).sum(:study_time) ])
-      end
+    days = ["月", "火", "水", "木", "金", "土", "日"]
 
-      current_user.study_categories.each do | study_category |
-        @learning_diaries_chart_data.push({ name: study_category.name, data: @daily_study_time })
+    # 未指定カテゴリのチャート用データ作成
+    category_blank_daily_study_time = []
+    if current_user.learning_diaries
+      days.each_with_index do | day, number |
+        category_blank_daily_study_time.push(["#{day}", current_user.learning_diaries.where(study_category: nil, study_day: (Date.today.beginning_of_week + number)).sum(:study_time) ])
       end
-      @study_categories_count = current_user.study_categories.group(:name).count
+      @learning_diaries_chart_data.push({ name: "カテゴリ未指定", data: category_blank_daily_study_time})
+      blank_category_learning_diaries_count = current_user.learning_diaries.where(study_day: Date.today.beginning_of_week..Date.current, study_category: nil ).count
+      study_categories_count = {"カテゴリ未指定" => blank_category_learning_diaries_count}
     end
+
+    # カテゴリ別チャート用データ作成
+    if current_user&.study_categories
+      current_user.study_categories.each  do | study_category |
+        daily_study_time = []
+        days.each_with_index do | day, number |
+          daily_study_time.push(["#{day}", study_category.learning_diaries.where(study_day: (Date.today.beginning_of_week + number)).sum(:study_time) ])
+        end
+        @learning_diaries_chart_data.push({ name: study_category.name, data: daily_study_time })
+      end
+      @study_categories_count = study_categories_count.merge(current_user.study_categories.joins(:learning_diaries).where(learning_diaries: { study_day: Date.today.beginning_of_week..Date.current }).group(:name).count)
+    end
+
+    # 1日の平均学習時間を算出
+    avg_daily_study_time = current_user.learning_diaries.where(study_day: Date.today.beginning_of_week..Date.current).sum(:study_time) / (Date.today.beginning_of_week..Date.current).count
+    @study_time_hh, @study_time_mm = avg_daily_study_time.divmod(60)
   end
 end
