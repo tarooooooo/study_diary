c0157067f452f9de71e3a012c85d4d580a811018
tarooooooo <ryutaromaeda83@gmail.com>
2022-10-02 11:14:05 +0900

    各チャート用データ作成のコメントを追加

--- app/controllers/public/top_page_controller.rb
+++ app/controllers/public/top_page_controller.rb
@@ -6,9 +6,12 @@ class Public::TopPageController < ::Public::BaseController
     # 未指定カテゴリのチャート用データ作成
     category_blank_daily_study_time = []
     if current_user.learning_diaries
+      # カラムチャート用データ追加（カテゴリなし）
       days.each_with_index do | day, number |
         category_blank_daily_study_time.push(["#{day}", current_user.learning_diaries.where(study_category: nil, study_day: (Date.today.beginning_of_week + number)).sum(:study_time) ])
       end
+
+      # カテゴリ別の円チャート用データ作成（カテゴリなし））
       @learning_diaries_chart_data.push({ name: "カテゴリ未指定", data: category_blank_daily_study_time})
       blank_category_learning_diaries_count = current_user.learning_diaries.where(study_day: Date.today.beginning_of_week..Date.current, study_category: nil ).count
       study_categories_count = {"カテゴリ未指定" => blank_category_learning_diaries_count}
@@ -16,6 +19,7 @@ class Public::TopPageController < ::Public::BaseController
 
     # カテゴリ別チャート用データ作成
     if current_user&.study_categories
+      # カテゴリ別のカラムチャート用データの追加
       current_user.study_categories.each  do | study_category |
         daily_study_time = []
         days.each_with_index do | day, number |
@@ -23,6 +27,7 @@ class Public::TopPageController < ::Public::BaseController
         end
         @learning_diaries_chart_data.push({ name: study_category.name, data: daily_study_time })
       end
+      # カテゴリ別の円チャートようデータ追加（カテゴリあり）
       @study_categories_count = study_categories_count.merge(current_user.study_categories.joins(:learning_diaries).where(learning_diaries: { study_day: Date.today.beginning_of_week..Date.current }).group(:name).count)
     end
 
