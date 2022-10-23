b19bd39e85134d417200765dd86cfa23c3e05300
tarooooooo <ryutaromaeda83@gmail.com>
2022-10-23 16:41:35 +0900

    ユーザープロフィール画像のリサイズ・task追加

--- app/uploaders/user_profile_uploader.rb
+++ app/uploaders/user_profile_uploader.rb
@@ -1,4 +1,5 @@
 class UserProfileUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
   storage :file
 
   def store_dir
@@ -7,5 +8,17 @@ class UserProfileUploader < CarrierWave::Uploader::Base
 
   def default_url
     "default.jpg"
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+  version :thumb do
+    process resize_to_fit: [500, 500]
+  end
+
+  version :small_thumb do
+    process resize_to_fit: [300, 300]
   end
 end
